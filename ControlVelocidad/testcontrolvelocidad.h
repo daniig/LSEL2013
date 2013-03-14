ac14d8009c61f65355669d45b0ae4f8e158c171b
senkzer0 <senkzer0@contempt.SKNET>
2013-03-14 23:20:04 +0100

    primera subida de la funcionalidad de control de velocidad

---
+++ ControlVelocidad/testcontrolvelocidad.h
@@ -0,0 +1,17 @@
+/*
+ * testcontrolvelocidad.h
+ *
+ *  Created on: 12/03/2013
+ *      Author: senkzer0
+ */
+
+#ifndef TESTCONTROLVELOCIDAD_H_
+#define TESTCONTROLVELOCIDAD_H_
+
+
+	void incrLinVel(int t_interval, char tren, char dir);
+	void decLinVel(int t_interval, char tren, char dir);
+	void incrEscVel(int t_step, char tren, char dir);
+	void decEscVel(int t_step, char tren, char dir);
+
+#endif /* TESTCONTROLVELOCIDAD_H_ */
